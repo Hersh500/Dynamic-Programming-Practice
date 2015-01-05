ab8094dcdee64d97fdef60324790608d4f814424
Hersh Sanghvi <hersh500@gmail.com>
2015-01-05 13:34:51 -0800

    Add description for knapsack weights problem

--- thief.py
+++ thief.py
@@ -1,3 +1,17 @@
+'''
+Suppose you are a computer scientist/art thief who has broken into a major art gallery. 
+All you have with you to haul out your stolen art is your knapsack which only holds W pounds of art, but for every piece of art you know its value and its weight. 
+Write a dynamic programming function to help you maximize your profit.
+Here is a sample problem for you to use to get started: Suppose your knapsack can hold a total weight of 20. 
+You have 5 items as follows:
+
+item     weight      value
+  1        2           3
+  2        3           4
+  3        4           8
+  4        5           8
+  5        9          10 '''
+
 class StolenItem:
 	def __init__(self, theID, theValue, theWeight):
 		self.ID = theID
